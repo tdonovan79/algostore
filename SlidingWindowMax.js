b1e0028b8bffaa7aa5cb1f53b31328d746f80f7b
tdonovan79 <tdonovan79@gmail.com>
2020-06-12 15:58:01 -0400

    better sliding window solution

--- SlidingWindowMax.js
+++ SlidingWindowMax.js
@@ -24,26 +24,48 @@
 // 1 <= nums.length <= 10^5
 // -10^4 <= nums[i] <= 10^4
 // 1 <= k <= nums.length
+// var maxSlidingWindow = function(nums, k) {
+//     let left = new Array(nums.length), right = new Array(nums.length), maxes = new Array()
+//     for(let i = 0 ; i < nums.length ; i++){//get max for each k window from left to right
+//         if(i % k === 0){//if first element in window, max is itself
+//             left[i] = nums[i]
+//         }
+//         else {//else max between this or previous element in window
+//             left[i] = Math.max(nums[i], left[i - 1])
+//         }
+//     }
+//     for(let i = nums.length - 1 ; i >= 0 ; i--){//get max for each k window from left to right
+//         if(i % k === 0 || i === nums.length - 1){//if first in window or last on array, itself
+//             right[i] = nums[i]
+//         }
+//         else {//else max between this or previous element in window
+//             right[i] = Math.max(nums[i], right[i + 1])
+//         }
+//     }
+//     for(let i = 0 ; i < nums.length - k + 1 ; i++){//get maxin each window by either max of left side of window coming from the right, or max of right side of window coming from left
+//         maxes[i] = Math.max(right[i], left[i + k - 1])
+//     }
+//     return maxes
+// };
+//**BETTER SOLUTION */
 var maxSlidingWindow = function(nums, k) {
-    let left = new Array(nums.length), right = new Array(nums.length), maxes = new Array()
-    for(let i = 0 ; i < nums.length ; i++){//get max for each k window from left to right
-        if(i % k === 0){//if first element in window, max is itself
-            left[i] = nums[i]
+    let queue = [0]//intitialize queue, must be kept largest to smallest
+    for(let i = 1 ; i < k ; i++){//loop through intial k values
+        while(queue.length && ((nums[queue[queue.length - 1]] < nums[i]))){
+            queue.pop()//while items in queue and last element smaller than current, pop
         }
-        else {//else max between this or previous element in window
-            left[i] = Math.max(nums[i], left[i - 1])
-        }
+        queue.push(i)//push current element onto queue
     }
-    for(let i = nums.length - 1 ; i >= 0 ; i--){//get max for each k window from left to right
-        if(i % k === 0 || i === nums.length - 1){//if first in window or last on array, itself
-            right[i] = nums[i]
+    let maxes = [nums[queue[0]]]//first max is beginining of queue
+    for(let fast = k, slow = 1 ; fast < nums.length ; fast++, slow++){//initalize values to start and end of window
+        while(queue.length && ((nums[queue[queue.length - 1]] < nums[fast]))){
+            queue.pop()//while items in queue and last element smaller than current, pop
         }
-        else {//else max between this or previous element in window
-            right[i] = Math.max(nums[i], right[i + 1])
+        while(queue[0] < slow){
+            queue.shift()//while first element is not in window, pop
         }
-    }
-    for(let i = 0 ; i < nums.length - k + 1 ; i++){//get maxin each window by either max of left side of window coming from the right, or max of right side of window coming from left
-        maxes[i] = Math.max(right[i], left[i + k - 1])
+        queue.push(fast)//push current element onto queue
+        maxes.push(nums[queue[0]])//add current max to maxes array
     }
     return maxes
 };
